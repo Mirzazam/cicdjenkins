ac6ec8c1ecf2e227db4862828a45cde669bda2ee
admin <admin@gmail.com>
2022-12-31 16:37:13 +0530

    indented error

--- userdata/jenkins-setup.sh
+++ userdata/jenkins-setup.sh
@@ -2,6 +2,7 @@
 sudo apt update
 sudo apt install openjdk-11-jdk -y
 sudo apt install maven -y
+sudo apt install awscli -y
 curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo tee \
   /usr/share/keyrings/jenkins-keyring.asc > /dev/null
 echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
